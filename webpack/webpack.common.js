87f037b60b23f88f4362abdbf3520affdfce7a68
Vlad Trishch <vlad.trishch@gmail.com>
2019-09-14 22:08:43 +0300

    Initial commit

---
+++ webpack/webpack.common.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack');
+const commonPaths = require('./paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: commonPaths.entryPath,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.(png|jpg|JPG|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.imagesFolder,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2|ttf|woff|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: commonPaths.fontsFolder,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      fonts: path.resolve(__dirname, '../src/fonts'),
+      components: path.resolve(__dirname, '../src/components'),
+    }
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: commonPaths.templatePath,
+    }),
+    new webpack.ProvidePlugin({
+      hot: ['react-hot-loader', 'hot'],
+    }),
+  ],
+};
+
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,24 @@
+const commonPaths = require('./paths');
+
+module.exports = {
+  mode: 'development',
+  output: {
+    filename: '[name].js',
+    path: commonPaths.outputPath,
+    chunkFilename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
